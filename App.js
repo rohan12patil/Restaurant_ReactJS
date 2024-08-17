2e0452cd9c57f03caea77d2f0b54eb0d263a870d
Rohan Patil <rohan12patil@gmail.com>
2024-08-17 19:00:43 +0100

    Header Component Added

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import logo from './assets/imgs/restaurant.png';
+/**
+ *
+ * Header
+ *  - Logo
+ *  - Navbar list
+ * Body
+ *  - Search Bar
+ *  - Restaurant Container
+ *  - Restaurant Card
+ *
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='logo-container'>
+        <img src={logo}></img>
+      </div>
+      <div className='navbar'>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 const AppLayout = () => {
   return (
     <div>
-      List All Components here <br /> Header <br /> Body <br /> Footer 🚀
+      <Header />
     </div>
   );
 };
