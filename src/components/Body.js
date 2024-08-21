f4d337b30a1ab869eedc1e35b361f06a0c065ac6
Rohan Patil <rohan12patil@gmail.com>
2024-08-21 18:28:19 +0100

    Adding Hooks - setState for filtering list by rating > 4

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,25 @@
 import { RestaurantCard } from './RestaurantCard';
 import { resList } from '../utils/mockData';
+import { useState } from 'react';
 
 export const Body = () => {
+  const [restaurantList, setrestaurantList] = useState(resList);
+
   return (
     <div className='body'>
       <div className='search-container'>Search</div>
+      <button
+        onClick={() => {
+          let filteredList = restaurantList.filter(
+            (res) => res.info.avgRating > 4
+          );
+          setrestaurantList(filteredList);
+        }}
+      >
+        Rating 4.x
+      </button>
       <div className='restaurant-container'>
-        {resList.map((res) => (
+        {restaurantList.map((res) => (
           <RestaurantCard resData={res.info} key={res.info.id} />
         ))}
       </div>
