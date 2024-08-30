d182d4f6533f50cfbba889c0975eba6978ac6f06
Rohan Patil <rohan12patil@gmail.com>
2024-08-30 03:09:36 +0100

    search functionality added

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,8 @@ import Shimmer from './shimmer';
 
 export const Body = () => {
   const [restaurantList, setrestaurantList] = useState([]);
+  const [searchText, setsearchText] = useState('');
+  const [filteredRestaurantList, setfilteredRestaurantList] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -13,24 +15,42 @@ export const Body = () => {
   const fetchData = async () => {
     const data = await fetch(RESTAURANT_LIST_URL);
     const result = await data.json();
-    console.log(
+
+    let resData =
       result.data.success.cards[1].card.card.gridElements.infoWithStyle
-        .restaurants
-    );
-    setrestaurantList(
-      result?.data?.success?.cards[1]?.card?.card?.gridElements?.infoWithStyle
-        ?.restaurants
-    );
+        .restaurants;
+
+    setrestaurantList(resData);
+    setfilteredRestaurantList(resData);
   };
 
   return restaurantList.length === 0 ? (
     <Shimmer />
   ) : (
     <div className='body'>
-      <div className='search-container'>Search</div>
+      <div className='search-container'>
+        <input
+          type='text'
+          value={searchText}
+          onChange={(e) => {
+            setsearchText(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            console.log(searchText);
+            let filteredList = restaurantList.filter((res) =>
+              res.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setfilteredRestaurantList(filteredList);
+          }}
+        >
+          Search
+        </button>
+      </div>
       <button
         onClick={() => {
-          let filteredList = restaurantList.filter(
+          let filteredList = filteredRestaurantList.filter(
             (res) => res.info.avgRating > 4.3
           );
           setrestaurantList(filteredList);
@@ -39,7 +59,7 @@ export const Body = () => {
         Rating 4.x
       </button>
       <div className='restaurant-container'>
-        {restaurantList.map((res) => (
+        {filteredRestaurantList.map((res) => (
           <RestaurantCard resData={res.info} key={res.info.id} />
         ))}
       </div>
